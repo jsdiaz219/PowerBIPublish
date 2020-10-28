1690bbf4ebd7e6238800c362d51ffc496db8ad92
jsdiaz219 <salvador.diaz@avec.com.ar>
2020-10-28 10:23:38 -0300

    added home page with some buttons

--- src/controllers/power.controllers.js
+++ src/controllers/power.controllers.js
@@ -6,8 +6,15 @@ const postVenta = (req, res, next) => {
     res.send('<iframe width="1300" height="660" src="https://app.powerbi.com/reportEmbed?reportId=b1bd1c51-40dc-46f3-b231-51b22045584d&autoAuth=true&ctid=0417ccb0-9706-4d62-8445-fde88422fa2a&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXBhYXMtMS1zY3VzLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9" frameborder="0" allowFullScreen="true"></iframe>')
 }
 
+const homePage = (req, res, next) => {
+    res.send(view)
+}
+
+const view ='<div><form method="get" action="/ventas"><button type="submit">Ventas</form></div>'+
+            '<div><form method="get" action="/postventa"><button type="submit">Postventas</form></div>'
 
 module.exports = {
     getVentas,
-    postVenta
+    postVenta,
+    homePage
 }
--- src/routes/power.routes.js
+++ src/routes/power.routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { getVentas, postVenta } = require('../controllers/power.controllers')
+const { getVentas, postVenta, homePage } = require('../controllers/power.controllers')
 
+router.get('/', homePage)
 router.get('/ventas', getVentas)
 router.get('/postventa', postVenta)
+
 
 module.exports = router
