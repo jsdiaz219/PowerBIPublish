e86b0c3089ae10eeb2dbeb52cf8f553a338015a4
jsdiaz219 <salvador.diaz@avec.com.ar>
2020-11-18 10:48:55 -0300

    modified postventa

--- src/controllers/power.controllers.js
+++ src/controllers/power.controllers.js
@@ -3,7 +3,7 @@ const getVentas = (req, res, next) =>{
 }
 
 const postVenta = (req, res, next) => {
-    res.send('<iframe width="1300" height="660" src="https://app.powerbi.com/reportEmbed?reportId=b1bd1c51-40dc-46f3-b231-51b22045584d&autoAuth=true&ctid=0417ccb0-9706-4d62-8445-fde88422fa2a&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXBhYXMtMS1zY3VzLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9" frameborder="0" allowFullScreen="true"></iframe>')
+    res.send('<iframe width="1300" height="660" src="https://app.powerbi.com/view?r=eyJrIjoiYWI0MDI0NzktNzI1ZS00NTUyLTk1NzMtYTQzMjg2M2VlODY4IiwidCI6IjA0MTdjY2IwLTk3MDYtNGQ2Mi04NDQ1LWZkZTg4NDIyZmEyYSJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>')
 }
 
 const homePage = (req, res, next) => {
